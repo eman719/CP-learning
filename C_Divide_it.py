543070d7066babe1e85412c63f2c81ad91712b77
eman719 <emanabdullaziz719@gmail.com>
2026-09-05 21:51:00 +0300

    submitted

--- C_Divide_it.py
+++ C_Divide_it.py
@@ -3,20 +3,19 @@ for _ in range (t):
     ans=0
     
     n=int(input())
-    while n!=1:
+    while n>1:
       if n%2==0:
-          n/=2
+          n = n//2
           ans+=1
       elif n%3==0:
-         n=(n/3)*2
+         n=(n//3)*2
          ans+=1
       elif n%5==0:
-         n=(n/5)*4
+         n=(n//5)*4
          ans+=1
       else:
+         ans= -1
          break
       
-    if n!=1:
-       print(-1)
-    else:
-       print(ans)
+    
+    print(ans)
